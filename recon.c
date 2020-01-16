b184425bc5b74e691ea5f9e08a0d2a3d82ef53ba
Paillamano <jair.torres@usach.cl>
2020-01-16 01:31:03 -0300

    Esctructura con arreglos.OK-17M(Completo)

--- recon.c
+++ recon.c
@@ -17,102 +17,148 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "func.h"
 #include "sorting.h"
 
-
+#define ARRAY_LEN 100000
 //ESTRUCTURAS(DEBIDO A LA CANTIDAD DE DATOS)
 
-struct nodo{
-    int n;
-    char x;
-}typedef nodo;
+struct lista_l{
+    double arrayA[ARRAY_LEN];
+    double arrayB[ARRAY_LEN];
+    struct lista_l *next;
+}typedef lista_l;
 
 //----------PROTOTIPADO----------//
 FILE*open_file(const char*filename, const char*apertura);//----------------------COMPLETO
 //void get_data(FILE*file, int arry[],const char* metodo="1by1");//----------------INCOMPLETO
 void append(char cadena[],char at_end);//-----------------------------------------COMPLETO
-void mostrar(node h);
-node Insertar(node h,double value);
 
+void Insert_data(lista_l*head, FILE* file, int *counter_node, int*counter_data);
+int data_counter(FILE* file);
+lista_l* init_list( FILE* file, int *counter_data);
+void clean_list(lista_l *head);
 //--------FIN PROTOTIPADO--------//
 int main(int argc, char*argv[])
 {
-    nodo *a;
-    ArrayList(nodo) *Datos = new_ArrayList(nodo,{ .n=7, .x='A'});//FUNCIONA
-    LinkedList(int) *lista = new_LinkedList(int,1,2,3,4,5,6);
-    printf("%i%c",lista->data,'\n');
-    Show_LinkedList(int,lista);
-    printf("%i  %c",(*Datos).data[0].n,(*Datos).data[0].x);
-    //print_f(); //arboles con reduccion de memoria
     printf("Abriendo archivo\n");
-    //FILE* num = open_file("numeros.txt","r");
     FILE* num = fopen("ArchivoA.tex","r");
-    node h = NULL;
     rewind(num);
-    double list[10];
-    printf("Iniciando buffer char\n");
-    char *char_buff = (char*)malloc(sizeof(char_buff));
-    printf("iniciando buffer float\n");
+    double list[ARRAY_LEN];
+    int peso = 1;
+    int data = 0;
+    //char *char_buff = (char*)malloc(sizeof(char_buff));
     double buffer;
-    printf("Iniciando ciclo\n");
-    for(unsigned int i = 0; i<10; i++)
-    {
-        fscanf(num,"%le",&buffer);
-        list[i] = buffer;
-        //h = Insertar(h,*buffer);
-        printf("%i ) %.7e \n",i,list[i]);
-    }
-    int aux = short_arry_len(list);
-    quicksort_double(list,0,aux-1);
-    printf("%.7e%c",list[0],'\n');
-    for(unsigned int e = 0; e<10; e++)
-    {
-        printf("%.7e%c",list[e],'\n');
-    }
+    //fflush(stdout);
+    printf("Contando elementos...\n");
+    clock_t _count = clock();
+    long int file_numbers = data_counter(num);
+    double _time_count = ((double)clock()-_count)/CLOCKS_PER_SEC;
+    printf("...OK. \n %li datos contados en el archivo (%f sec)", file_numbers, _time_count);
+    rewind(num);
+    lista_l * pointer;
+    clock_t start = clock();
+    pointer = init_list(num,&data);
+    Insert_data(pointer, num,&peso,&data);
+    double _time_in = ((double)clock() - start)/CLOCKS_PER_SEC;
+    printf("\n|          Lista A          |          Lista B        |\n");
+    /**for(int i = 0;i<ARRAY_LEN;i++){
+        printf("|-------------------------------------------------------|\n");
+        printf("|       %.7e       |       %.7e       |\n",(*pointer).arrayA[i],(*pointer).arrayB[i]);
+        printf("|-------------------------------------------------------|\n");
+    }**/
+    printf("...OK \n Tiempo transcurrido: %f \n Total de datos contados: %i \n Total de datos ordenados: %i \n Peso de la estructura(en bytes): %zu \n",_time_in,data,data,sizeof(lista_l));
+    printf(" Numero de nodos enlazados: %d \n Peso total(en bytes): %zu (%f mb)\n",peso, sizeof(lista_l) * peso, (sizeof(lista_l) * peso)* 1e-6);
+    printf("Liberando memoria...");
+    clock_t free_mem = clock();
+    clean_list(pointer);
+    double final_freemem = ((double)clock()-free_mem)/CLOCKS_PER_SEC;
+    printf("Memoria liberada con exito \n Peso actual del puntero: %zu", sizeof(pointer));
     fclose(num);
 }
 
+lista_l* init_list( FILE* file, int *counter_data){
+    lista_l *head = malloc(sizeof(lista_l));
+    (*head).next = NULL;
+    int index;
+    double buffer;
+    //printf("Lista_A => init\n");
+    for(index = 0; index<ARRAY_LEN;index++){
+         fscanf(file,"%le",&buffer);
+         (*head).arrayA[index] = buffer;
+         *counter_data+=1;
+    }
+    //printf("Lista_B => init\n");
+    for(index = 0; index<ARRAY_LEN;index++){
+         fscanf(file,"%le",&buffer);
+         (*head).arrayB[index] = buffer;
+         *counter_data+=1;
+    }
+    //printf("Iniciando quicksort con: %li datos",short_arry_len((*head).arrayA)+short_arry_len((*head).arrayB));
+    quicksort_double((*head).arrayA,0,ARRAY_LEN-1);
+    quicksort_double((*head).arrayB,0,ARRAY_LEN-1);
+    return head;
+}
 
-
-
-void mostrar(node h)
-{
-    node aux;
-    aux = h;
-    while(aux != NULL){
-        printf("%.7e%c",(*aux).n,'\n');
-        aux = (*aux).next;
+void clean_list(lista_l *head){
+    if((*head).next == NULL){
+        free(head);
+    }else{
+        clean_list((*head).next);
+    }
+}
+void Insert_data(lista_l*head, FILE* file, int *counter_node, int *counter_data){
+    if(!file){
+        printf("ERROR.");
+    }else{
+        lista_l * aux;
+        aux = head;
+        int index;
+        double buffer;
+        do{
+            lista_l *temp = malloc(sizeof(lista_l));
+            //printf("Lista_A => body\n");
+            for(index = 0; index<ARRAY_LEN; index++){
+                if(!feof(file)){
+                    fscanf(file,"%le",&buffer);
+                    (*temp).arrayA[index] = buffer;
+                    *counter_data+=1;
+                }
+            }
+            //printf("Lista_B => body\n");
+            for(index = 0; index<ARRAY_LEN; index++){
+                if(!feof(file)){
+                    fscanf(file,"%le",&buffer);
+                    (*temp).arrayB[index] = buffer;
+                    *counter_data+=1;
+                }
+            }
+            (*temp).next = NULL;
+            (*aux).next = temp;
+            aux = temp;
+            temp = NULL;
+            quicksort_double((*aux).arrayA,0,ARRAY_LEN-1);
+            quicksort_double((*aux).arrayB,0,ARRAY_LEN-1);
+            *counter_node+=1;
+            //free(temp);
+        }while(!feof(file));
     }
 }
 
-node init_node()
-{
-    node temp;
-    temp = (node)malloc(sizeof(struct LinkedList));
-    (*temp).next = NULL;
-    return temp;
-}
-
-node Insertar(node h,double value)
-{
-    node temp,p;
-    temp = init_node();
-    (*temp).n = value;
-    if(h==NULL)
-    {
-        h = temp;
-    }
-    else
-    {
-        p = h;
-        while((*p).next !=NULL)
-        {
-            p = (*p).next;
+int data_counter(FILE* file){
+    if(!file){
+        printf("ERROR, el archivo debe estar abierto\n");
+        return 0;
+    }else{
+        double buffer;
+        long int counter = 0;
+        while(!feof(file)){
+            fscanf(file,"%le",&buffer);
+            counter++;
         }
-        (*p).next = temp;
+        return counter;
     }
-    return h;
 }
 
 
